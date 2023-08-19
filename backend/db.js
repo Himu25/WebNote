59ca743048be823f88efe8f30b65775b6f8071f9
Himanshu Singh <99968348+Himu25@users.noreply.github.com>
2023-08-20 01:40:59 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,3 @@ mongoose.connect(process.env.HOST,{useNewUrlParser: true}
 }).catch(()=>{
     console.log("No connection");
 })
-
-// username :   himanshu638684
-// password :  1oEmcg1reMXVoBwV
-//url : mongodb+srv://himanshu638684:1oEmcg1reMXVoBwV@cluster0.44wrne4.mongodb.net/WebNote
